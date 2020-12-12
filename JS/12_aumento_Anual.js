62543e5b04fdd0c6d31d56bd78921342891c99a6
Nestor <nreupo@gmail.com>
2020-12-12 12:14:08 -0500

    Actualización

--- JS/12_aumento_Anual.js
+++ JS/12_aumento_Anual.js
@@ -1,10 +1,12 @@
 function salario(){
-    alert('El sueldo Inicial es de S/ 1500.00')
-    let Anio = Number;
+    alert('El sueldo Inicial es de S/1500.00')
+    let Anio = Number(prompt("Ingrese la cantidad de Años"));
     let salario = 1500;
     for (Anio = 1; Anio <= 6; Anio++) {
         salario = 1500 * 1.10 ** Anio;
-        console.log("Salario en el año ", Anio, " es de ", salario.toFixed(2));
+        alert("Salario en el año " +Anio +" es de " + salario.toFixed(2)); 
     }
+    
 }
+
 salario();
---
+++ JS/13_Calificaciones.js
@@ -0,0 +1,23 @@
+function clase(){
+let i = new Number();
+let apro = new Number();
+let desa = new Number();
+apro = 0;
+desa = 0;
+
+let n = Number(prompt("Ingrese la cantidad de alumnos"));
+
+for (i = 1; i <= n; i++) {
+    let nota = Number(prompt("Ingrese la nota del alumno", i));
+    if (nota > 12) {
+        apro = apro + 1;
+    } else {
+        desa = desa + 1;
+    }
+}
+
+alert("los Aprobados son: " +apro + " y desaprobados son: "+desa );
+
+
+}
+clase();

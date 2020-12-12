0bc7a6a0ef0f8bb0480698e71fc9327600700e19
Nestor <nreupo@gmail.com>
2020-12-12 11:48:07 -0500

    Actualización

---
+++ JS/12_aumento_Anual.js
@@ -0,0 +1,11 @@
+function salario(){
+    alert('El sueldo Inicial es de S/ 1500.00')
+    let Anio = prompt('Ingrese Años trabajados');
+    let salario = 1500;
+    for (Anio = 1; Anio <= 6; Anio++) {
+        salario = 1500 * 1.10 ** Anio;
+        alert("Salario en el año ", Anio, " es de ", salario.toFixed(2));
+    }
+}
+
+salario();
